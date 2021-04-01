5a2cde3a256b730363da0e866b0b221a05df267d
Eduardo <eduardo.rene.parra@gmail.com>
2021-04-01 15:01:26 +0200

    Initial commit, Detta är början.

---
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -0,0 +1,9 @@
+package stepDefinitions;
+
+
+
+       // System.setProperty("webdriver.chrome.driver", "D:\\Eduardo\\Selenium\\chromedriver.exe");
+        //driver = new ChromeDriver();
+        //driver.get("ENTER WEBB ADRESS!!!");
+
+
