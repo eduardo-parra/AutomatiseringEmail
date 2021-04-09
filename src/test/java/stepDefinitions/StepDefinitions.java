3453d8c72c33687fc2ba46ac19416d6c5c43cac5
Eduardo <eduardo.rene.parra@gmail.com>
2021-04-09 14:02:56 +0200

    Klar allt är GRÖNT!!!!!

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -11,17 +11,17 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.commons.lang3.RandomStringUtils;
 
-import java.util.UUID;
+
 import java.util.concurrent.TimeUnit;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class StepDefinitions{
+public class StepDefinitions {
 
 
     public static void click(WebDriver driver, By by) {
 
-        (new WebDriverWait(driver,30)).until(ExpectedConditions.elementToBeClickable(by));
+        (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(by));
 
         driver.findElement(by).click();
 
@@ -31,52 +31,53 @@ public class StepDefinitions{
     private WebElement result;
 
     @Given("i am on the email regrestration page in {string} https:\\/\\/login.mailchimp.com\\/signup\\/")
-    public void i_am_on_the_email_regrestration_page_in_https_login_mailchimp_com_signup(String browser){
+    public void i_am_on_the_email_regrestration_page_in_https_login_mailchimp_com_signup(String browser) {
         WebDrivers creator = new WebDrivers();
         driver = creator.createBrowser(browser);
         driver.get("https://login.mailchimp.com/signup/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
     @Given("I have input {string}")
     public void i_have_input(String emails) {
         WebElement input = driver.findElement(By.id("email"));
 
-        if(emails.equals("noEmail")){
+        if (emails.equals("noEmail")) {
             input.sendKeys("");
-        }
-        else{input.sendKeys("hungrig@hotmail.com");
+        } else {
+            input.sendKeys("hungrig@hotmail.com");
         }
 
     }
+
     @Given("I have also input {string}")
     public void i_have_also_input(String username) {
         WebElement input = driver.findElement(By.id("new_username"));
 
 
-        if(username.equals("100longUser")){
+        if (username.equals("100longUser")) {
 
             String result = RandomStringUtils.randomAlphabetic(105);
 
             WebElement user = driver.findElement(By.id("new_username"));
             user.sendKeys(result);
-        }
-        else if(username.equals("alredyUser")){
+        } else if (username.equals("alredyUser")) {
             WebElement user = driver.findElement(By.id("new_username"));
             user.sendKeys(username);
 
 
-        }
-        else if(username.equals("validUser")){
+        } else if (username.equals("validUser")) {
 
 
-           String result = RandomStringUtils.randomAlphabetic(8);
+            String result = RandomStringUtils.randomAlphabetic(8);
 
             WebElement user = driver.findElement(By.id("new_username"));
             user.sendKeys(result);
 
         }
     }
+
     @Given("I have also input pasword {string}")
     public void i_have_input_pasword(String LöSen) {
         WebElement input = driver.findElement(By.id("new_password"));
@@ -84,14 +85,17 @@ public class StepDefinitions{
     }
 
     @When("I press signup")
-    public void i_press_signup(){
-        WebElement signUp = driver.findElement(By.id("create-account"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", signUp);
-        signUp.click();
+    public void i_press_signup() {
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+                driver.findElement(By.id("create-account")));
+
+        click(driver, By.id("create-account"));
 
     }
-    @Then("the result should be  on the screen")
-    public void the_result_should_be_on_the_screen(String message){
+
+    @Then("the result should be {string} on the screen")
+    public void the_result_should_be_on_the_screen(String message) {
 
         if (message.equals("Check your email")) {
             result = driver.findElement(By.cssSelector(".\\!margin-bottom--lv3"));
@@ -105,6 +109,8 @@ public class StepDefinitions{
         System.out.println(result.getAttribute("innerText") + " = " + message);
 
         driver.quit();
+
+
     }
 
 
--- src/test/resources/Features/Steps.feature
+++ src/test/resources/Features/Steps.feature
@@ -1,6 +1,8 @@
 Feature:Mail Testning
-Background:
-  Given i am on the email regrestration page in "chrome" https://login.mailchimp.com/signup/
+
+  Background:
+    Given i am on the email regrestration page in "chrome" https://login.mailchimp.com/signup/
+
   @mytag
   Scenario Outline:User sign up
     Given I have input <email>
@@ -9,10 +11,10 @@ Background:
     When I press signup
     Then the result should be <message> on the screen
     Examples:
-      |email|username|password|message |
-      |"emailValid"|"validUser"|"LöSen1=?"|  "Check your email"  |
-      |"emailValid"| "100longUser"|"LöSen1=?"|"Enter a value less than 100 characters long"|
-    |"emailValid"  | "alredyUser" |"LöSen1=?"|"Another user with this username already exists. Maybe it's your evil twin. Spooky."|
-    | "noEmail" | "validUser" |  "LöSen1=?"  |"Please enter a value"|
+      | email        | username      | password   | message                                                                              |
+      | "emailValid" | "validUser"   | "LöSen1=?" | "Check your email"                                                                   |
+      | "emailValid" | "100longUser" | "LöSen1=?" | "Enter a value less than 100 characters long"                                        |
+      | "emailValid" | "alredyUser"  | "LöSen1=?" | "Another user with this username already exists. Maybe it's your evil twin. Spooky." |
+      | "noEmail"    | "validUser"   | "LöSen1=?" | "Please enter a value"                                                               |
 
 
